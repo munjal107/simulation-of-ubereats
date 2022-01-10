3ced019b2347bc4eed09824e8b645dd513c2d6d0
Munjal <munjal1997@gmail.com>
2022-01-10 12:04:37 -0500

    c2

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -1,7 +1,6 @@
 const config = {
   secret: "cmpe-273-lab1-secret-key",
   frontendURL: 'http://localhost:3000',
-  // frontendURL : "http://3.17.130.248:3000"
   // serverURL : 'http://localhost:5500'
 }
 
